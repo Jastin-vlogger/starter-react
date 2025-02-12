9ca10774fcbba183b867984409e0a28219efb8f4
Jastin <jastin.kj@neutrinos.co>
2025-02-12 09:10:05 +0530

    add initial project setup with .gitignore, package.json, and app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const heading = React.createElement('h1', {
+    key: 'hi'
+}, 'Heading 1');
+const span = React.createElement('span', {
+    key: 'span'
+}, 'span 1');
+const divElement = React.createElement('div', {
+    key: 'd'
+}, [heading, span]);
+const root = createRoot(document.getElementById('root'));
+root.render(divElement)
