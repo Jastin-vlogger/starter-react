18345033cdac388e840df9dfe4670aa01e53159b
Jastin <jastin.kj@neutrinos.co>
2025-02-15 19:17:14 +0530

    feat: add comments for hot module reloading and update package.json with browserslist

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-import React from 'react';
+/**
+ * HMR - HOT MODULE RELOADING
+ * File watcher algorithm c++
+ */
+
+import React from 'react';   
 import { createRoot } from 'react-dom/client';
 
 const heading = React.createElement('h1', {
     key: 'hi'
-}, 'Heading 1');
+}, 'Heading 1 text');
 const span = React.createElement('span', {
     key: 'span'
 }, 'span 1');
